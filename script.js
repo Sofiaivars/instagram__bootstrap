08b9950beb0ec2b17ac8249ba69d97a37507d88c
Sofiaivars <sofiaivars1@gmail.com>
2025-03-28 19:00:00 +0100

    Creamos un proyecto de instagram utilizando bootstrap

---
+++ script.js
@@ -0,0 +1,18 @@
+{/* <script>
+const feedGrid = document.getElementById('feed-grid');
+const feedSingle = document.getElementById('feed-single');
+const cardView = document.getElementById('card-view');
+const gridView = document.getElementById('grid-view');
+
+// Cuando se seleccione la vista de cuadrícula
+feedGrid.addEventListener('change', () => {
+    cardView.classList.add('d-none');
+    gridView.classList.remove('d-none');
+});
+
+// Cuando se seleccione la vista de tarjeta
+feedSingle.addEventListener('change', () => {
+    cardView.classList.remove('d-none');
+    gridView.classList.add('d-none');
+});
+</script> */}
